2f4e109e54c4e4952bc1b807740afa1986158bd7
peshka <ilyapeshekhonov@gmail.com>
2020-06-04 00:44:16 +0300

    add link column

--- src/urlshortener/admin.py
+++ src/urlshortener/admin.py
@@ -7,7 +7,7 @@ from .models import URL
 class URLAdminModel(ModelAdmin):
     list_display = ('__str__', 'link',)
 
-    def link_field(self, obj):
+    def link(self, obj):
         return '<a href="/%s">/%s</a>' % (obj.slug, obj.url_field)
-    link_field.allow_tags = True
-    link_field.short_description = 'Link'
+    link.allow_tags = True
+    link.short_description = 'Link'
